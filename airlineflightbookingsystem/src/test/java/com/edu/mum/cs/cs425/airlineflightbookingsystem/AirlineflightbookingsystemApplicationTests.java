3f22bbfa26ffa52d2806626ccc11c5f8e883d155
HenokElias1567 <henokeliasinfo@gmail.com>
2019-10-02 02:13:24 -0500

    First setup commit

---
+++ airlineflightbookingsystem/src/test/java/com/edu/mum/cs/cs425/airlineflightbookingsystem/AirlineflightbookingsystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.edu.mum.cs.cs425.airlineflightbookingsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AirlineflightbookingsystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
